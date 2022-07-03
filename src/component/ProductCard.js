847525a90ac6e5df34201c768e9bf67e521c659c
Acep Awaludin <acep.awal.sosmed@gmail.com>
2022-07-03 16:46:07 +0700

    Membuat Component ProductCard

--- src/component/ProductCard.js
+++ src/component/ProductCard.js
@@ -1,10 +1,19 @@
 import React from 'react'
+import Products from './../assets/img/product1.png'
 
 function ProductCard() {
     return (
-        <div>
-            <h1>Ini adalah Product Card</h1>
-        </div>
+        <div className='container'>
+            <h5 className='text-orange'>Product</h5>
+            <div className="card bg-grey-800" style={{ width: 241 }}>
+                <img src={Products} className="card-img-top" alt="Products" />
+                <div className="card-body">
+                    <h5 className="card-title text-orange">Mouse</h5>
+                    <p className="card-text">Rp. 500.000</p>
+                    <p className="card-text">Stock: 600</p>
+                </div>
+            </div>
+        </div >
     )
 }
 
