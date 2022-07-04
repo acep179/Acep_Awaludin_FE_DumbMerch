bb07ed77e4d5ba8a38b3fb984160f2d02f8d20eb
Acep Awaludin <acep.awal.sosmed@gmail.com>
2022-07-04 14:54:13 +0700

    Menerapkan State pada Halaman Category

--- src/component/ListCategoryTable.js
+++ src/component/ListCategoryTable.js
@@ -1,33 +1,15 @@
 import React from 'react'
 
-function ListCategoryTable() {
+function ListCategoryTable(props) {
     return (
-        <div className='container'>
-            <p>List Category</p>
-            <table className='table-dark-striped w-100'>
-                <tr>
-                    <th>No</th>
-                    <th>Category Name</th>
-                    <th>Action</th>
-                </tr>
-                <tr>
-                    <td>1</td>
-                    <td>Mouse</td>
-                    <td>
-                        <p className='btn btn-success my-0 me-3'>Edit</p>
-                        <p className='btn btn-danger my-0'>Delete</p>
-                    </td>
-                </tr>
-                <tr>
-                    <td>2</td>
-                    <td>Keyboard</td>
-                    <td>
-                        <p className='btn btn-success my-0 me-3'>Edit</p>
-                        <p className='btn btn-danger my-0'>Delete</p>
-                    </td>
-                </tr>
-            </table>
-        </div>
+        <tr>
+            <td>{props.id}</td>
+            <td>{props.categoryName}</td>
+            <td>
+                <p className='btn btn-success my-0 me-3'>Edit</p>
+                <p className='btn btn-danger my-0'>Delete</p>
+            </td>
+        </tr>
     )
 }
 
--- src/pages/category.js
+++ src/pages/category.js
@@ -1,11 +1,36 @@
 import React from 'react'
 import { ListCategoryTable, NavBar } from './../component'
 
+let data = [
+    {
+        id: 1,
+        categoryName: 'Mouse',
+    },
+    {
+        id: 2,
+        categoryName: 'Keyboard',
+    },
+]
+
 function Category() {
     return (
         <div>
             <NavBar />
-            <ListCategoryTable />
+            <div className='container'>
+                <p>List Category</p>
+                <table className='table-dark-striped w-100'>
+                    <tr>
+                        <th>No</th>
+                        <th>Category Name</th>
+                        <th>Action</th>
+                    </tr>
+                    {data.map((item) => {
+                        return (
+                            <ListCategoryTable id={item.id} categoryName={item.categoryName} />
+                        )
+                    })}
+                </table>
+            </div >
         </div>
     )
 }
