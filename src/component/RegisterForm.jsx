c36dac5ca4ec8ba6ab637edfdd811fba63cb392d
Acep Awaludin <acep.awal.sosmed@gmail.com>
2022-07-16 20:06:35 +0700

    Mengupdate component RegisterForm agar dapat terhubung dengan server menggunakan react-query, menambahkan handleSubmit

--- src/component/RegisterForm.jsx
+++ src/component/RegisterForm.jsx
@@ -1,11 +1,14 @@
 import React, { useState } from 'react'
-import { Link } from 'react-router-dom'
+import { useNavigate } from 'react-router-dom'
 import { useMutation } from 'react-query';
 import { API } from '../config/api';
 
 
 function RegisterForm() {
+
+    let navigate = useNavigate()
     const [message, setMessage] = useState(null);
+
     const [form, setForm] = useState({
         name: '',
         email: '',
@@ -35,10 +38,12 @@ function RegisterForm() {
 
             const response = await API.post('/register', body, config);
 
+            navigate('/login')
+
         } catch (error) {
             const alert = (
                 <div class="alert alert-danger" role="alert">
-                    A simple danger alert—check it out!
+                    Failed
                 </div>
             );
             setMessage(alert);
@@ -46,25 +51,38 @@ function RegisterForm() {
         }
     });
 
-
-
     return (
         <div className='col-5 d-flex align-items-center'>
             <div className="card bg-grey-800 p-3">
                 <h2 className='mb-3'>Register</h2>
-                <form>
+                <form onSubmit={(e) => handleSubmit.mutate(e)}>
                     <div className='input-group input-group-lg mb-3'>
-                        <input type="text" className='form-control bg-grey-700' placeholder='Name' name='name' />
+                        <input
+                            type="text"
+                            className='form-control bg-grey-700'
+                            placeholder='Name'
+                            name='name'
+                            value={name}
+                            onChange={handleChange} />
                     </div>
                     <div className='input-group input-group-lg mb-3'>
-                        <input type="email" className='form-control bg-grey-700' placeholder='Email' name='email' />
+                        <input
+                            type="email"
+                            className='form-control bg-grey-700'
+                            placeholder='Email'
+                            name='email'
+                            value={email}
+                            onChange={handleChange} />
                     </div>
                     <div className='input-group input-group-lg mb-3'>
-                        <input type="password" className='form-control bg-grey-700' placeholder='Password' name='password' />
+                        <input
+                            type="password"
+                            className='form-control bg-grey-700' placeholder='Password'
+                            name='password'
+                            value={password}
+                            onChange={handleChange} />
                     </div>
-                    <Link to="/login">
-                        <button className='btn btn-orange w-100' type="submit">Register</button>
-                    </Link>
+                    <button className='btn btn-orange w-100' type="submit">Register</button>
                 </form>
             </div>
         </div>
