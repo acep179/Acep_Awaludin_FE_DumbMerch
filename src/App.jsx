d7c008a7fa45e8e54e1c3ae404fe8bc36b75af28
Acep Awaludin <acep.awal.sosmed@gmail.com>
2022-07-18 08:29:08 +0700

    Mengupdate LoginForm dengan integrasi ke backend, Membuat userContext, Memperbaiki App.js dan index.js

--- src/App.jsx
+++ src/App.jsx
@@ -1,25 +1,86 @@
 // import logo from './logo.svg';
 // import './App.css';
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { useContext, useEffect } from 'react';
+import { Routes, Route, useNavigate } from 'react-router-dom';
+import { UserContext } from './context/userContext';
 import { Category, Complain, DetailProduct, EditCategory, EditProduct, Home, Login, Product, Profile, Register } from './pages'
 
+import { API, setAuthToken } from './config/api';
+
+if (localStorage.token) {
+  setAuthToken(localStorage.token);
+}
+
 function App() {
+
+  let navigate = useNavigate();
+
+  const [state, dispatch] = useContext(UserContext);
+
+  useEffect(() => {
+    if (localStorage.token) {
+      setAuthToken(localStorage.token);
+    }
+
+    // Redirect Auth
+    if (state.isLogin === false) {
+      navigate("/login");
+    } else {
+      if (state.user.status === 'admin') {
+        navigate('/product-admin');
+      } else if (state.user.status === 'customer') {
+        navigate('/');
+      }
+    }
+  }, [state]);
+
+
+  const checkUser = async () => {
+    try {
+      const response = await API.get('/check-auth');
+
+      // If the token incorrect
+      if (response.status === 404) {
+        return dispatch({
+          type: 'AUTH_ERROR',
+        });
+      }
+
+      // Get user data
+      let payload = response.data.data.user;
+      // Get token from local storage
+      payload.token = localStorage.token;
+
+      // Send data to useContext
+      dispatch({
+        type: 'USER_SUCCESS',
+        payload,
+      });
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  useEffect(() => {
+    if (localStorage.token) {
+      checkUser();
+    }
+  }, []);
+
   return (
 
-    <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/category" element={<Category />} />
-        <Route path="/complain" element={<Complain />} />
-        <Route path="/detail-product" element={<DetailProduct />} />
-        <Route path="/edit-category" element={<EditCategory />} />
-        <Route path="/edit-product" element={<EditProduct />} />
-        <Route path="/login" element={<Login />} />
-        <Route path="/product" element={<Product />} />
-        <Route path="/profile" element={<Profile />} />
-        <Route path="/register" element={<Register />} />
-      </Routes>
-    </BrowserRouter>
+    <Routes>
+      <Route path="/" element={<Home />} />
+      <Route path="/category" element={<Category />} />
+      <Route path="/complain" element={<Complain />} />
+      <Route path="/detail-product" element={<DetailProduct />} />
+      <Route path="/edit-category" element={<EditCategory />} />
+      <Route path="/edit-product" element={<EditProduct />} />
+      <Route path="/login" element={<Login />} />
+      <Route path="/product" element={<Product />} />
+      <Route path="/profile" element={<Profile />} />
+      <Route path="/register" element={<Register />} />
+    </Routes>
 
   );
 }
--- src/component/LoginForm.jsx
+++ src/component/LoginForm.jsx
@@ -1,21 +1,104 @@
-import React from 'react'
-import { Link } from 'react-router-dom'
+import React, { useState, useContext } from 'react'
+import { useNavigate } from 'react-router-dom'
+import { useMutation } from 'react-query';
+import { API } from '../config/api';
+import { UserContext } from '../context/userContext';
+
 
 function LoginForm() {
+
+  let navigate = useNavigate();
+  const [state, dispatch] = useContext(UserContext);
+  const [message, setMessage] = useState(null);
+
+  const [form, setForm] = useState({
+    email: '',
+    password: '',
+  });
+
+  const { email, password } = form;
+
+  const handleChange = (e) => {
+    setForm({
+      ...form,
+      [e.target.name]: e.target.value,
+    });
+  };
+
+  const handleSubmit = useMutation(async (e) => {
+    try {
+      e.preventDefault();
+
+      const config = {
+        headers: {
+          'Content-type': 'application/json',
+        },
+      };
+
+      const body = JSON.stringify(form);
+      const response = await API.post('/login', body, config);
+
+      if (response?.status === 200) {
+
+        dispatch({
+          type: 'LOGIN_SUCCESS',
+          payload: response.data.data,
+        });
+
+        if (response.data.data.status === 'admin') {
+          navigate('/complain-admin');
+        } else {
+          navigate('/');
+        }
+
+        const alert = (
+          <div class="alert alert-danger" role="alert">
+            Failed
+          </div>
+        );
+
+        setMessage(alert);
+      }
+
+    } catch (error) {
+      const alert = (
+        <div class="alert alert-danger" role="alert">
+          Login failed
+        </div>
+      );
+      setMessage(alert);
+      console.log(error);
+    }
+  });
+
+
   return (
     <div className='col-5 d-flex align-items-center'>
       <div className="card bg-grey-800 p-3">
         <h2>Login</h2>
-        <form>
-          <div className='input-group input-group-lg'>
-            <input type="text" className='form-control bg-grey-700' aria-label="Sizing example input" aria-describedby="inputGroup-sizing-lg" placeholder='Email' />
+        {message && message}
+        <form onSubmit={(e) => handleSubmit.mutate(e)}>
+          <div className='input-group input-group-lg mt-3'>
+            <input
+              type="email"
+              className='form-control bg-grey-700'
+              placeholder='Email'
+              value={email}
+              onChange={handleChange}
+              name='email'
+            />
           </div>
-          <div className='input-group input-group-lg'>
-            <input type="password" className='form-control bg-grey-700' aria-label="Sizing example input" aria-describedby="inputGroup-sizing-lg" placeholder='Password' />
+          <div className='input-group input-group-lg mt-3'>
+            <input
+              type="password"
+              className='form-control bg-grey-700'
+              placeholder='Password'
+              value={password}
+              onChange={handleChange}
+              name='password'
+            />
           </div>
-          <Link to="/">
-            <button className='btn btn-orange w-100' type="submit">Login</button>
-          </Link>
+          <button className='btn btn-orange w-100 mt-3' type="submit">Login</button>
         </form>
       </div>
     </div>
