51fcb9f9e92f818f419a515015b06b388b0bdc4a
Acep Awaludin <acep.awal.sosmed@gmail.com>
2022-07-27 13:52:37 +0700

    Menambahkan event message untuk menampilkan pesan pada halaman Compalain Admin dan Customer menggunakan socket.io

--- src/pages/ComplainAdmin.jsx
+++ src/pages/ComplainAdmin.jsx
@@ -1,19 +1,23 @@
-import { React, useEffect, useState } from 'react'
-import { NavBar, Contact } from '../component'
+import { React, useEffect, useState, useContext } from 'react'
+import { NavBar, Contact, Chat } from '../component'
 import { io } from "socket.io-client"
+import { UserContext } from '../context/userContext'
 
 let socket
 function ComplainAdmin() {
 
     const [contact, setContact] = useState(null)
     const [contacts, setContacts] = useState([])
+    const [messages, setMessages] = useState([])
+    const [state] = useContext(UserContext)
 
-    const loadContact = () => {
+    const loadContacts = () => {
         socket.emit("load customer contacts")
 
         socket.on("customer contacts", (data) => {
 
-            let dataContacts = data.filter(item => item.status !== 'admin')
+            let dataContacts = data.filter((item) => (item.status !== 'admin') &&
+                (item.recipientMessage.length > 0 || item.senderMessage.length > 0))
 
             dataContacts = dataContacts.map((item) => ({
                 ...item,
@@ -28,10 +32,20 @@ function ComplainAdmin() {
         socket = io('http://localhost:5000', {
             auth: {
                 token: localStorage.getItem("token") // we must set options to get access to socket server
+            },
+            query: {
+                id: state.user.id
             }
         })
 
-        loadContact()
+        socket.on("new message", () => {
+            console.log("contact", contact)
+            console.log("triggered", contact?.id)
+            socket.emit("load messages", contact?.id)
+        })
+
+        loadContacts()
+        loadMessages()
 
         socket.on("connect_error", (err) => {
             console.error(err.message); // not authorized
@@ -40,11 +54,40 @@ function ComplainAdmin() {
         return () => {
             socket.disconnect()
         }
-    }, [])
+    }, [messages])
 
     const onClickContact = (data) => {
-        console.log(data);
         setContact(data)
+        socket.emit("load messages", data.id)
+    }
+
+    const loadMessages = (value) => {
+
+        socket.on("messages", (data) => {
+            if (data.length > 0) {
+                const dataMessages = data.map((item) => ({
+                    idSender: item.sender.id,
+                    message: item.message
+                }))
+                console.log(dataMessages);
+                setMessages(dataMessages)
+            }
+            loadContacts()
+            const chatMessages = document.getElementById("chat-messages")
+            chatMessages.scrollTop = chatMessages?.scrollHeight
+        })
+    }
+
+    const onSendMessage = (e) => {
+        if (e.key === 'Enter') {
+            const data = {
+                idRecipient: contact.id,
+                message: e.target.value
+            }
+
+            socket.emit("send messages", data)
+            e.target.value = ""
+        }
     }
 
     return (
@@ -58,9 +101,14 @@ function ComplainAdmin() {
                             clickContact={onClickContact}
                             contact={contact} />
                     </div>
-                    {/* <div style={{ maxHeight: '89.5vh' }} className="col-md-9 px-0">
-                        <Chat contact={contact} />
-                    </div> */}
+                    <div style={{ maxHeight: '89.5vh' }} className="col-md-9 px-0">
+                        <Chat
+                            contact={contact}
+                            messages={messages}
+                            user={state.user}
+                            sendMessage={onSendMessage}
+                        />
+                    </div>
                 </div>
             </div>
         </div>
--- src/pages/ComplainCustomer.jsx
+++ src/pages/ComplainCustomer.jsx
@@ -1,22 +1,26 @@
-import { React, useEffect, useState } from 'react'
-import { NavBar, Contact } from '../component'
+import { React, useContext, useEffect, useState } from 'react'
+import { NavBar, Contact, Chat } from '../component'
 import { io } from "socket.io-client"
+import { UserContext } from '../context/userContext'
 
 let socket
 function ComplainCustomer() {
 
     const [contact, setContact] = useState(null)
     const [contacts, setContacts] = useState([])
+    const [messages, setMessages] = useState([])
+    const [state, dispatch] = useContext(UserContext)
 
     const loadContact = () => {
         socket.emit("load admin contact")
 
         socket.on("admin contact", (data) => {
+
             console.log(data);
 
             const dataContact = {
                 ...data,
-                message: "Click here to start message"
+                message: messages.length > 0 ? messages[messages.length - 1].message : "Click here to start message"
             }
             setContacts([dataContact])
         })
@@ -26,23 +30,62 @@ function ComplainCustomer() {
         socket = io('http://localhost:5000', {
             auth: {
                 token: localStorage.getItem("token") // we must set options to get access to socket server
+            },
+            query: {
+                id: state.user.id
             }
         })
 
-        loadContact()
+        socket.on("new message", () => {
+            console.log("contact", contact)
+            console.log("triggered", contact?.id)
+            socket.emit("load messages", contact?.id)
+        })
 
         socket.on("connect_error", (err) => {
             console.error(err.message); // not authorized
         });
 
+        loadContact()
+        loadMessages()
+
         return () => {
             socket.disconnect()
         }
-    }, [])
+
+    }, [messages])
 
     const onClickContact = (data) => {
-        console.log(data);
         setContact(data)
+        socket.emit("load messages", data.id)
+    }
+
+    const loadMessages = (value) => {
+
+        socket.on("messages", async (data) => {
+            if (data.length > 0) {
+                const dataMessages = data.map((item) => ({
+                    idSender: item.sender.id,
+                    message: item.message
+                }))
+                console.log(dataMessages);
+                setMessages(dataMessages)
+            }
+            const chatMessages = document.getElementById("chat-messages")
+            chatMessages.scrollTop = chatMessages?.scrollHeight
+        })
+    }
+
+    const onSendMessage = (e) => {
+        if (e.key === 'Enter') {
+            const data = {
+                idRecipient: contact.id,
+                message: e.target.value
+            }
+
+            socket.emit("send messages", data)
+            e.target.value = ""
+        }
     }
 
     return (
@@ -57,9 +100,14 @@ function ComplainCustomer() {
                             contact={contact}
                         />
                     </div>
-                    {/* <div style={{ maxHeight: '89.5vh' }} className="col-md-9 px-0">
-                        <Chat contact={contact} />
-                    </div> */}
+                    <div style={{ maxHeight: '89.5vh' }} className="col-md-9 px-0">
+                        <Chat
+                            contact={contact}
+                            messages={messages}
+                            user={state.user}
+                            sendMessage={onSendMessage}
+                        />
+                    </div>
                 </div>
             </div>
         </div>
