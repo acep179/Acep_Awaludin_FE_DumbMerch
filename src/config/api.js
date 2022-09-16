311002ac2fccf2dea5123279a1ccf8a6f2173c22
Acep Awaludin <acep.awal.sosmed@gmail.com>
2022-09-16 22:09:15 +0700

    Mengganti nama variabel base url

--- src/config/api.js
+++ src/config/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 export const API = axios.create({
-    baseURL: process.env.SERVER_BASEURL,
+    baseURL: process.env.REACT_APP_BASEURL,
 });
 
 //. Menyimpan token ke dalam Header
